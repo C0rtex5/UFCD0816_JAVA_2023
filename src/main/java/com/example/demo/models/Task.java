aa04607e64f2eb1f40d318e25bdeb26c283430b2
jhonnynetworker <ebusiness.latino@live.com>
2023-12-05 17:19:30 +0000

    correcao erro amaro

--- src/main/java/com/example/demo/models/Task.java
+++ src/main/java/com/example/demo/models/Task.java
@@ -1,5 +1,48 @@
 package com.example.demo.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private boolean isCompleted;
+
+    // Getters and setters
+    public String getDescription() {
+        return description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.isCompleted = completed;
+    }
+
+    // Constructors
+    // Other necessary methods
+}
+
+
+/* package com.example.demo.models;
+
 @jakarta.persistence.Entity
 public class Task {
 
@@ -31,4 +74,4 @@ public class Task {
     }
     public void setCompleted(Object completed2) {
     }
-}
+} */
